941cd7aab27b2a4bf05d998dac63a573573a648f
jsungl <wotjd931026@gmail.com>
2023-04-13 17:27:37 +0900

    스프링 MVC - 컨트롤러 클래스 통합

---
+++ src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,49 @@
+package hello.servlet.web.springmvc.v2;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    // 회원 등록 폼
+    @RequestMapping("/new-form")
+    public ModelAndView newForm(){
+        return new ModelAndView("new-form");
+    }
+
+    // 회원 저장
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response){
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mav = new ModelAndView("save-result");
+        mav.addObject("member", member); // 모델 데이터 추가
+        return mav;
+    }
+
+    // 회원 목록 조회
+    @RequestMapping
+    public ModelAndView members(){
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mav = new ModelAndView("members");
+        mav.addObject("members", members);
+        return mav;
+    }
+
+
+}
